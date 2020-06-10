db08f52d3a9cbb3e46e520ca976a51e65825094e
Mark Tamarov <googlcraftlol@gmail.com>
2020-06-10 09:43:49 +0300

    still working on that f shit

--- src/header/header.php
+++ src/header/header.php
@@ -1,27 +1,28 @@
 <link rel="stylesheet" type="text/css" href="src/header/header.css" />
 
 <nav class="nv-fix navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#page1">TLC</a>
+    <a class="navbar-brand" href="/#page1">TLC</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav w-100">
-            <li class="nav-item dropdown ml-auto">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Грузоперевозки </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#page2">Грузоперевозки</a>
-                    <a class="dropdown-item" href="#page3">Преимущества</a>
-                    <a class="dropdown-item" href="#page4">Список Услуг</a>
-                    <a class="dropdown-item" href="#page5">Автопарк</a>
-                    <a class="dropdown-item" href="#page6">Тарифы</a>
-                    <a class="dropdown-item" href="#page8">Контакты</a>
-                </div>
-            </li>
-        </ul>
-    </div>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav w-100">
+                <li class="nav-item dropdown ml-auto">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Грузоперевозки </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#page2">Грузоперевозки</a>
+                        <a class="dropdown-item" href="#page3">Преимущества</a>
+                        <a class="dropdown-item" href="#page4">Список Услуг</a>
+                        <a class="dropdown-item" href="#page5">Автопарк</a>
+                        <a class="dropdown-item" href="#page6">Тарифы</a>
+                        <a class="dropdown-item" href="#page8">Контакты</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
     </div>
   </nav>
 
